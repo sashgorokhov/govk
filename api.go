4a96fbe3d0f0413e055bc06de4729ab21256d45f
sashgorokhov <sashgorokhov@gmail.com>
2016-08-08 13:09:43 +0300

    Add logger: log api requests (#1)

--- api.go
+++ api.go
@@ -2,24 +2,31 @@ package govk
 
 import (
 	"github.com/Jeffail/gabs"
+	"github.com/Sirupsen/logrus"
 	"github.com/levigross/grequests"
+	"io/ioutil"
 )
 
 const default_version string = "5.50"
 const base_api_url string = "https://api.vk.com/method/"
 
+var ApiLogger = logrus.Logger{
+	Level:     logrus.InfoLevel,
+	Formatter: new(logrus.TextFormatter),
+	Hooks:     make(logrus.LevelHooks),
+	Out:       ioutil.Discard,
+}
+
 type Api struct {
-	Version string
+	Version      string
 	Access_token string
 }
 
-
-func NewApi(access_token string) (*Api) {
+func NewApi(access_token string) *Api {
 	return &Api{Version: default_version, Access_token: access_token}
 }
 
-
-func (a *Api) prepare_params (params map[string]string) *grequests.RequestOptions {
+func (a *Api) prepare_params(params map[string]string) *grequests.RequestOptions {
 	if params == nil {
 		params = make(map[string]string)
 	}
@@ -27,16 +34,26 @@ func (a *Api) prepare_params (params map[string]string) *grequests.RequestOption
 	if a.Access_token != "" {
 		params["access_token"] = a.Access_token
 	}
-	return &grequests.RequestOptions{Params:params}
+	return &grequests.RequestOptions{Params: params}
 }
 
 func (a *Api) RawRequest(method string, params map[string]string) (*grequests.Response, error) {
 	url := base_api_url + method
-	return grequests.Get(url, a.prepare_params(params))
+	prepared_params := a.prepare_params(params)
+	request_logger := ApiLogger.WithFields(logrus.Fields{
+		"url":    url,
+		"params": prepared_params.Params,
+	})
+	request_logger.Infoln("Making request")
+	response, err := grequests.Get(url, prepared_params)
+	request_logger.WithFields(logrus.Fields{
+		"status":   response.StatusCode,
+		"response": response.String(),
+	}).Infoln("Request made")
+	return response, err
 }
 
-
-func (a *Api) AbstractRequest (method string, params map[string]string) (*gabs.Container, error) {
+func (a *Api) AbstractRequest(method string, params map[string]string) (*gabs.Container, error) {
 	response, err := a.RawRequest(method, params)
 	if err != nil {
 		return nil, err
@@ -44,8 +61,7 @@ func (a *Api) AbstractRequest (method string, params map[string]string) (*gabs.C
 	return gabs.ParseJSONBuffer(response.RawResponse.Body)
 }
 
-
-func (a *Api) StructRequest (method string, params map[string]string, user_struct interface{}) (error) {
+func (a *Api) StructRequest(method string, params map[string]string, user_struct interface{}) error {
 	response, err := a.RawRequest(method, params)
 	if err != nil {
 		return err
